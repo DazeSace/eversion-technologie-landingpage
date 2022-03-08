dd4d6092377e537222f3d878dc1d771fafd04f8c
dazesace <dazemcblaze@gmail.com>
2022-03-08 04:35:12 +0100

    customers

---
+++ components/Customers.js
@@ -0,0 +1,94 @@
+import Image from "next/image";
+import {AiFillStar} from "react-icons/ai";
+
+
+const Customers = () => {
+    return (
+        <div>
+            <p className={'text-3xl text-center font-poppins mt-20'}>Unsere Kund:innen</p>
+            <div
+                className={'content flex flex-col lg:flex-row space-y-8 lg:space-y-0 lg:justify-around mt-16'}>
+                <div className={'flex flex-col rounded-xl lg:w-1/4 shadow-xl bg-xblue p-6'}>
+                    <div className={'flex flex-col'}>
+                        <div className={'flex justify-between'}>
+                            <div className={'flex'}>
+                                <div className={'h-12 w-12 rounded-full'}>
+                                    <Image src={'/images/man.png'} width={100} height={100}/>
+                                </div>
+                                <div className={'ml-2'}>
+                                    <p className={'text-xl font-poppins'}>Viezh Robert</p>
+                                    <p className={'text-sm'}>Ravensburg, Deutschland</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className={'flex justify-end mt-4'}>
+                        <p>4/5</p>
+                        <div className={'mt-1 ml-2'}>
+                            <AiFillStar/>
+                        </div>
+                    </div>
+                    <p className={'mt-1'}>“Wow... Ich bin so glücklich, dass ich diese Analyse gemacht habe. Endlich
+                        weiss ich, dass meine
+                        Schuhe meine Wadenkrämpfe verursachen. Seit ich die Insole täglich trage, kann ich endlich
+                        wieder in Ruhe schlafen ohne ständig von schmerzhaften Krämpfen geweckt zu werden.</p>
+                </div>
+                <div className={'flex flex-col rounded-xl lg:w-1/4 shadow-xl bg-xblue p-6'}>
+                    <div className={'flex flex-col'}>
+                        <div className={'flex justify-between'}>
+                            <div className={'flex'}>
+                                <div className={'h-12 w-12 rounded-full'}>
+                                    <Image src={'/images/girl.png'} width={100} height={100}/>
+                                </div>
+                                <div className={'ml-2'}>
+                                    <p className={'text-xl font-poppins'}>Yessica Christy</p>
+                                    <p className={'text-sm'}>München, Deutschland</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className={'flex justify-end mt-4'}>
+                        <p>5/5</p>
+                        <div className={'mt-1 ml-2'}>
+                            <AiFillStar/>
+                        </div>
+                    </div>
+                    <p className={'mt-1'}>“Ich liebe die Insoles. Sie haben mich
+                        überzeugt weil sie 100% individualisiert
+                        und perfekt auf mich und meinen Schuh
+                        angepasst sind.”</p>
+                </div>
+                <div className={'flex flex-col rounded-xl lg:w-1/4 shadow-xl bg-xblue p-6'}>
+                    <div className={'flex flex-col'}>
+                        <div className={'flex justify-between'}>
+                            <div className={'flex'}>
+                                <div className={'h-12 w-12 rounded-full'}>
+                                    <Image src={'/images/juliana.png'} width={100} height={100}/>
+                                </div>
+                                <div className={'ml-2'}>
+                                    <p className={'text-xl font-poppins'}>Juliana Weiß</p>
+                                    <p className={'text-sm'}>Kaiserslautern, Deutschland</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className={'flex justify-end mt-4'}>
+                        <p>4.5/5</p>
+                        <div className={'mt-1 ml-2'}>
+                            <AiFillStar/>
+                        </div>
+                    </div>
+                    <p className={'mt-1'}>“Ich war erst 23 Jahre alt als ich das erste Mal Knieprobleme bekam. Dass
+                        diese Schmerzen in den Schuhen entstehen können, hätte ich nie gedacht. Mit der anschaulichen
+                        3D-Darstellung und der verständlichen Erklärung konnte ich die Analyse sehr gut nachvollziehen
+                        .”</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Customers
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import HelpinYa from "../components/HelpinYa";
 import Analysis from "../components/Analysis";
 import AnalysisTiles from "../components/AnalysisTiles";
 import Eval from "../components/Eval";
+import Customers from "../components/Customers";
 
 
 export default function Home() {
@@ -13,6 +14,15 @@ export default function Home() {
             <title>Eversion Technologies</title>
             <meta name="description" content="Eversion Technologies - Ganganalyse von Zuhause"/>
             <meta name="keywords" content="Ganganalyse, Gesundheit, Füße, Schuhe, Fußfehlstellung"/>
+            <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+            <link rel="manifest" href="/favicons/site.webmanifest"/>
+            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#ffffff"/>
+            <link rel="shortcut icon" href="/favicons/favicon.ico"/>
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="msapplication-config" content="/favicons/browserconfig.xml"/>
+            <meta name="theme-color" content="#ffffff"/>
         </Head>
         <main className={'font-poppins-default'}>
             <NavBar/>
@@ -21,7 +31,7 @@ export default function Home() {
             <Analysis/>
             <AnalysisTiles/>
             <Eval/>
-
+            <Customers/>
         </main>
     </div>)
 }
