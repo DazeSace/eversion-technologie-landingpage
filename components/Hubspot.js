c9e795136cb7b89eb0d1f97e30bdc23b8eec3d4f
dazesace <dazemcblaze@gmail.com>
2022-03-14 19:33:16 +0100

    swag

--- components/Hubspot.js
+++ components/Hubspot.js
@@ -3,39 +3,29 @@ import {useEffect} from "react";
 
 const Hubspot = () => {
     useEffect(() => {
-            const script = document.createElement('script');
-            script.src = "//js-eu1.hsforms.net/forms/shell.js";
-            document.body.appendChild(script);
+        const script = document.createElement('script');
+        script.src = "//js-eu1.hsforms.net/forms/shell.js";
+        document.body.appendChild(script);
 
-            script.addEventListener('load', () => {
-                if (window.hbspt) {
-                    window.hbspt.forms.create({
-                            region: "eu1",
-                            portalId: "25437971",
-                            formId: "03f76453-a6a0-44af-a0a4-c132606a4c38",
-                            target: '#hub',
-                            onFormReady: () => {
-                                const elements = document.getElementsByClassName('hbspt-form');
-                                while (elements.length > 0) {
-                                    elements[0].parentNode.removeChild(elements[0]);
-                                }
-                            }
-                        }
-                    )
-                }
-            })
-        }
-    )
+        script.addEventListener('load', () => {
+            if (window.hbspt) {
+                window.hbspt.forms.create({
+                    region: "eu1",
+                    portalId: "25437971",
+                    formId: "03f76453-a6a0-44af-a0a4-c132606a4c38",
+                    target: '#hub'
+                })
+            }
+        })
+    }, [])
 
-    return (
-        <div className={'font-poppins-default'}>
-            <p className={'text-lg font-poppins'}>Vielen Dank für dein Interesse!</p>
-            <p className={'mt-4'}>Leider sind wir noch nicht gelauncht. Trag dich ein und bleib auf dem Laufenden. </p>
-            <p>Wir melden uns bei dir, sobald es etwas Neues gibt.</p>
-            <div className={'mt-6'} id={'hub'}/>
-            <p>Wir senden dir eine Bestätigungsmail zu, bitte bestätige diese. Die E-Mail kann möglicherweise im
-                Spam-Ordner gelandet sein</p>
-        </div>
-    )
+    return (<div className={'font-poppins-default'}>
+        <p className={'text-lg font-poppins'}>Vielen Dank für dein Interesse!</p>
+        <p className={'mt-4'}>Leider sind wir noch nicht gelauncht. Trag dich ein und bleib auf dem Laufenden. </p>
+        <p>Wir melden uns bei dir, sobald es etwas Neues gibt.</p>
+        <div className={'lg:w-2/3 mt-6'} id={'hub'}/>
+        <p>Wir senden dir eine Bestätigungsmail zu, bitte bestätige diese. Die E-Mail kann möglicherweise im
+            Spam-Ordner gelandet sein</p>
+    </div>)
 }
 export default Hubspot
